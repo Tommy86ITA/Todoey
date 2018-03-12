6c1da8c104540629f48af67a20e4907e28244044
Thomas Amaranto <thomasamaranto@hotmail.com>
2018-03-12 23:04:53 +0100

    Implement category

---
+++ Todoey/Controller/TodoListViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Thomas Amaranto on 09/03/18.
+//  Copyright © 2018 Thomas Amaranto. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    
+    //MARK: - dichiarazione variabili di istanza:
+    
+   
+   var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    let defaults = UserDefaults.standard
+    
+    // MARK: - viewDidLoad Function:
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            
+            itemArray = items
+        }
+    
+    }
+
+    //MARK: - didReceiveMemoryWarning function
+    //
+    //    override func didReceiveMemoryWarning() {
+    //        super.didReceiveMemoryWarning()
+    //        // Dispose of any resources that can be recreated.
+    //    }
+    
+    
+    //MARK: - metodi di tableView Datasource:
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    //MARK: - metodi delegati di tableView
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print (itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    //MARK: - azione aggiunta nuovo oggetto
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Aggiungi nuova voce a Todoey", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Aggiungi voce", style: .default) { (action) in
+            // cosa avviene quando l'utente clicca sul bottone "Aggiungi voce" nella UIAlert
+            
+            self.itemArray.append(textField.text ?? "Nuova voce")           //aggiungo il nuovo oggetto all'array
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
+            self.tableView.reloadData()                                     //ricarico i dati nella tableView
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Crea una nuova voce"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+
+
+    
+
+}
+
